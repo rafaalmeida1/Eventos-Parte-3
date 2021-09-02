562b47fc5be014d50ae26d3d47b4ab046a68d3a6
rafaalmeida1 <rafaempresarial312@gmail.com>
2021-09-02 18:32:32 -0300

    aula feita

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("init");
+function loaded(){ //serve para colocar o script antes do body, para assim o script ser carregado só depois que o body for carregado
+                    //momento de usar isso é quando eu quiser pegar um elemento depois que o body for carregado, porém o link do script estar antes
+    let t = document.getElementById("titulo"); //quando o body for carregado, o h1 vai existir, e ai sim consigo dar t.innerHTML e pega o h1
+
+    // t.innerHTML = "rrr";
+
+    //t.onclick = mudarText; //posso passar a função sem os parenteses, pois se colocar os parenteses aqui dentro, ele vai executar a função aqui nesse momento, e não lá no HTML
+
+    //segunda maneira de passar onclick aqui pelo js
+
+    t.addEventListener("mouseover", mudarText); //isso faz a mesma coisa, o "click" faz a mesma coisa que onclick
+    t.addEventListener("mouseout", mouseout);
+    //     quando tiver o click, ele modifica o texto com a função mudarText
+    //     posso passar outros eventos aqui, como onmouseover, e é tudo minúsculo mesmo, não é onmouseOver não
+}
+
+//primeira maneira de passar a função onclick pelo js, e não lá no HTML
+
+function mudarText(){
+    this.innerHTML = "Novo Texto"; //o this vai ser exatamente o objeto que está usando o onclick
+}
+
+function mouseout(){
+    this.innerHTML = "teste";
+}
