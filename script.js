25e9ca1fc3c5d112984621564c32473b8d99f504
rafaalmeida1 <rafaempresarial312@gmail.com>
2021-09-02 18:35:41 -0300

    Modify aula feita

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function loaded(){ //serve para colocar o script antes do body, para assim o scr
 
     // t.innerHTML = "rrr";
 
-    //t.onclick = mudarText; //posso passar a função sem os parenteses, pois se colocar os parenteses aqui dentro, ele vai executar a função aqui nesse momento, e não lá no HTML
+    //primeira maneira de passar onclick pelo js
+    //t.onclick = mudarText;  //posso passar a função sem os parenteses, pois se colocar os parenteses aqui dentro, ele vai executar a função aqui nesse momento, e não lá no HTML
 
     //segunda maneira de passar onclick aqui pelo js
 
@@ -13,8 +14,6 @@ function loaded(){ //serve para colocar o script antes do body, para assim o scr
     //     quando tiver o click, ele modifica o texto com a função mudarText
     //     posso passar outros eventos aqui, como onmouseover, e é tudo minúsculo mesmo, não é onmouseOver não
 }
-
-//primeira maneira de passar a função onclick pelo js, e não lá no HTML
 
 function mudarText(){
     this.innerHTML = "Novo Texto"; //o this vai ser exatamente o objeto que está usando o onclick
